423bd401fd4927760f1d98d3b1b17cf25f844833
Andrew Anthony <a.anthony1835@gmail.com>
2021-08-25 16:51:13 -0700

    created the database, installed express and mysql as npm packages

--- schema.sql
+++ schema.sql
@@ -2,8 +2,12 @@ DROP DATABASE IF EXISTS moviesDB;
 
 CREATE DATABASE moviesDB;
 
+USE moviesDB;
+
 CREATE TABLE movies(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NULL,
     PRIMARY KEY (id)
-)
+);
+
+INSERT INTO movies (name) VALUES ("La La Land"), ("Blockers"), ("Moulin Rouge"), ("Lord of the Rings"), ("Kill Bill"), ("Coco"), ("Wall-E"), ("Your Name")
